69e066105e3017a35caade83d23b5624a474650f
limsc <roko786@naver.com>
2025-03-11 16:49:11 +0900

    20250311_2

--- myproject/app/src/main/java/bitcamp/myapp/listener/WebInitListener.java
+++ myproject/app/src/main/java/bitcamp/myapp/listener/WebInitListener.java
@@ -1,4 +1,30 @@
 package bitcamp.myapp.listener;
 
-public class WebInitListener {
+import bitcamp.myapp.config.AppConfig;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration;
+
+public class WebInitListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // ServletContext:
+        ServletContext ctx = sce.getServletContext();
+
+        // 1) DispatcherServlert이 사용할 IoC 컨테이너(Bean 컨테이너) 준비
+        AnnotationConfigWebApplicationContext beanContainer = new AnnotationConfigWebApplicationContext();
+        beanContainer.register(AppConfig.class);
+        beanContainer.refresh();
+
+        // 2) DispacherServlet을 준비
+        DispatcherServlet servlet = new DispatcherServlet(beanContainer);
+        ServletRegistration.Dynamic registration = ctx.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/app/*");
+    }
 }
